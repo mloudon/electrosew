ead464397b3e9f2fd76205d7cb0ece2dc8c6a3ae
Drew Roos <mrgriscom@gmail.com>
2017-12-03 21:08:15 +0200

    patterns

--- nazgul-gw/nazgul-gw.ino
+++ nazgul-gw/nazgul-gw.ino
@@ -25,7 +25,8 @@
 
 
 #define STRAND_LENGTH 36
-#define BRIGHTNESS 127
+//#define BRIGHTNESS 127
+#define BRIGHTNESS 255
 
 /**
  * Whether the pattern is mirrored, or reversed. This is useful for scarfs where 
@@ -50,9 +51,9 @@
 
 //#define RAINBOW
 //#define SEAPUNK
-//#define INDIGO
+#define INDIGO
 //#define BLUE_GREEN
-#define HEART
+//#define HEART
 
 #if defined (RAINBOW)
   #define HUE_START 0
@@ -86,6 +87,19 @@
 
 CRGB leds[STRAND_LENGTH];
 
+const int MAX_BRIGHTNESS = 255;  // max 256; don't set too high or arduino gets stressed and can't be easily reflashed
+const bool USE_GAMMA_CORRECTION = true;  // more linear brightness in color ramp, but expensive. disable if framerate is too low on long strips
+const float GAMMA = 1.8;
+
+float cycle(float t, float period, float min, float max) {
+  return .5*(min+max) - .5*(max-min) * cos(t / period * (2*PI));
+}
+
+int lum(float t, float period) {
+  float k = cycle(t, period, 0, 1);
+  return MAX_BRIGHTNESS * (USE_GAMMA_CORRECTION ? pow(k, GAMMA) : k);
+}
+
 void setup() {
   FastLED.addLeds<APA102, 5, 4, BGR>(leds, STRAND_LENGTH).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness( BRIGHTNESS );
@@ -94,9 +108,52 @@ void setup() {
 
 int offset = 0;
 
+int mode = 0;
+int num_modes = 3;
+
 void loop(){
+
+    // needs debouncing
+    if (digitalRead(9) == LOW)
+    {
+      mode = (mode + 1) % num_modes;
+    }
+
   
   unsigned long t = millis();
+
+  if (mode == 0) {
+
+  // rainbow
+  for (int i = 0; i < STRAND_LENGTH; i++) {
+    leds[i] = CHSV(10*i + 1*millis(), 255, 255);
+  }
+
+  } else if (mode == 1) {
+
+  // breathe
+  float bright = cycle(millis(), 12000, 0, 1);
+  bright = pow(bright, 25);
+  int v = (.07+.93*bright)*255;
+  for (int i = 0; i < STRAND_LENGTH; i++) {
+    leds[i] = CHSV(175, 255, v);
+  }
+
+  } else if (mode == 2) {
+
+  // variable pulse
+  float len = 1. / cycle(millis(), 20000, 1./(STRAND_LENGTH * 2.5), 1/5.);
+  float offset = millis() / 1000.;
+  for (int i = 0; i < STRAND_LENGTH; i++) {
+    float bright = cycle(i + offset*len, len, 0, 1);
+    bright = pow(bright, 3);
+    int v = (.05+.95*bright)*255;
+    leds[i] = CHSV(170, 255, v);
+  }
+
+  }
+
+  /* perlin
   byte color = getClock(t, 2);
   byte pulse = inoise8(t / 4.) * .5;
   byte drift = getClock(t, 3);
@@ -142,12 +199,13 @@ void loop(){
       leds[STRAND_LENGTH - 1 - loc] = CHSV(hue, 255 * SATURATION, value);
     #endif
   }
+  */
 
   // delay 20ms to give max 50fps. Could do something fancier here to try to 
   // hit exactly 60fps (or whatever) if possible, but takinng another millis()
   // reading, but not sure if there would be a point to that. 
   FastLED.show(); // display this frame
-  FastLED.delay(20);
+  //FastLED.delay(20);
 }
 
 
